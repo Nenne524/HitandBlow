2bb9653c37e0617ff0dab9cfe5555c6ee6c8998e
山根愛実 <yamaneami@yamaneaiminoMacBook-Pro.local>
2023-06-16 20:53:33 +0900

    image change

--- omikuzi/OmikuziImageView.swift
+++ omikuzi/OmikuziImageView.swift
@@ -10,22 +10,19 @@ import SwiftUI
 struct daikiti: View {
     var body: some View {
         Image("daikiti")
-            .frame(width:300, height: 300)
-            .clipShape(Circle())
+            .resizable()
     }
 }
 struct tyukiti: View {
     var body: some View {
         Image("tyukiti")
-            .frame(width:300, height: 300)
-            .clipShape(Circle())
+            .resizable()
     }
 }
 struct syokiti: View {
     var body: some View {
         Image("syokiti")
-            .frame(width:300, height: 300)
-            .clipShape(Circle())
+            .resizable()
     }
 }
 
